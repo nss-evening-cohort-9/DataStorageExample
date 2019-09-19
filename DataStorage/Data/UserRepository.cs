09a7aa3013bda5922ee1c56a7cd3b065b5a73d58
Nathan Gonzalez <ncgonz@gmail.com>
2019-09-18 21:08:04 -0500

    Add project files.

---
+++ DataStorage/Data/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStorage.Data
+{
+    class UserRepository
+    {
+        static List<User> _users = new List<User>();
+
+        public List<User> GetAll()
+        {
+            //get the users from the "database"
+            return _users;
+        }
+
+        public void Add(User userToAdd)
+        {
+            //add a user to the "database"
+            userToAdd.Id = Guid.NewGuid();
+
+            _users.Add(userToAdd);
+        }
+
+        public void Remove(User userToRemove)
+        {
+            //remove the user
+            _users.Remove(userToRemove);
+        }
+
+        public User Update(Guid userId, string firstName, string lastName)
+        {
+            //update the user
+            var user = _users.First(x => x.Id == userId);
+
+            user.FirstName = firstName;
+            user.LastName = lastName;
+
+            return user;
+        }
+
+    }
+
+    internal class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DataStorage/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using DataStorage.Data;
+
+namespace DataStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var userRepository = new UserRepository();
+
+                Console.WriteLine("Enter the user's first name");
+                var firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter the user's last name");
+                var lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter the user's password");
+                var password = Console.ReadLine();
+
+                var user = new User
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Password = password
+                };
+
+                userRepository.Add(user);
+                
+                Console.WriteLine("Would you like to add another? (y/n)");
+                var response = Console.ReadLine();
+
+                if (response != "y")
+                {
+                    break;
+                }
+            }
+
+            var otherRepository = new UserRepository();
+
+            var users = otherRepository.GetAll()
+                .Select(user => $"{user.FirstName} {user.LastName} has a password of {user.Password}");
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
